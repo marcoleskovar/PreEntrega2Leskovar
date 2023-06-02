117eb35c2ccde188f4260d9e1c643e725e774f06
Marco Leskovar <leskovarmarco@gmail.com>
2023-06-02 19:35:23 -0300

    version 1.1(HighOrderFunctionsI)

--- js/main.js
+++ js/main.js
@@ -53,7 +53,7 @@ const QueCantidad = (respuestaQueProductos) =>{
         const Prod =  respuestaQueProductos[rep]
         let ingreseCantidad = 0
         while (isNaN(ingreseCantidad) || (ingreseCantidad)<=0){     //Mientras que ingreseCantidad sea isNaN o <= 0, que la condicion sea true y se ejecute el while
-            ingreseCantidad = prompt('Que cantidad de ' + Prod.nombre + 's quiere?\nPrecio por unidad: $' + Prod.precio + '\n\nProductos Pedidos:\n' + lista)
+            ingreseCantidad = parseFloat(prompt('Que cantidad de ' + Prod.nombre + 's quiere?\nPrecio por unidad: $' + Prod.precio + '\n\nProductos Pedidos:\n' + lista))
             if (isNaN(ingreseCantidad) || (ingreseCantidad)<=0){        //Por eso cuando ingresas un valor isNaN o <= 0 la condicion es true por eso te manda la alerta y vuelve a iniciar el ciclo hasta que ingreses algo distinto de isNaN o 0
                 alert (ingreseCantidad + ' no es un numero valido. Reingrese')
             }
@@ -71,28 +71,24 @@ console.log(respuestaQueCantidad)
 const QuePrecio = (respuestaQueCantidad) =>{
     let lista = ''
     let lista2 = ''
-    let sumaPrecios = 0
-    let precioDescuento = 0
-    let precioIVA = 0
     let salida = []
-    for (let i=0; i<respuestaQueCantidad.length; i++){      //Se crea un bucle en base al numero de elementos que nos dejo el array de la funcion anterior
-        const QueElemento = respuestaQueCantidad [i]        //En esta constante se guarda un numero de elemento especifico que va creciendo segun el numero de iteraciones
-        const PrecioTotal = QueElemento.Cantidad * QueElemento.PrecioPorUnidad      //En esta constante se multiplica el precio de la unidad por la cantidad
-        lista += '-' + QueElemento.Producto + ': $' + QueElemento.PrecioPorUnidad + '\n'        //Se crea una lista que contiene todos los productos y sus precios por unidad
-        lista2 += QueElemento.Producto + 's (' + QueElemento.Cantidad + '): $' + PrecioTotal + '\n'     //Se crea una lista que te muestra el precio tuyo en base a la cantidad y el producto elegido
-        sumaPrecios += PrecioTotal      //En cada iteracion se suma el PrecioTotal para mostrarte el valor bruto de todo junto
-        precioDescuento= ((sumaPrecios * 0.9) * ( 1 + (IVA /100)))
-        precioIVA= (sumaPrecios * ( 1 + (IVA /100)))
-    }
-    if (sumaPrecios >=10000){       //Si tu compra es mayor a 10.000 te descuenta el 10% sino solo te lo deja el precio bruto + IVA
+    const sumaPrecios2 = respuestaQueCantidad.reduce((acumulador, elemento) =>{
+        const CantidadXPrecio= elemento.Cantidad * elemento.PrecioPorUnidad;
+        lista += '-' + elemento.Producto + ': $' + elemento.PrecioPorUnidad + '\n'
+        lista2 += elemento.Producto + 's (' + elemento.Cantidad + '): $' + CantidadXPrecio + '\n'
+        return acumulador + CantidadXPrecio
+    } , 0)
+    let precioDescuento= ((sumaPrecios2 * 0.9) * ( 1 + (IVA /100)))
+    let precioIVA= (sumaPrecios2 * ( 1 + (IVA /100)))
+    if (sumaPrecios2 >=10000){       //Si tu compra es mayor a 10.000 te descuenta el 10% sino solo te lo deja el precio bruto + IVA
         alert('Su compra supera los $10000 por eso tiene un descuento del 10%\n' + 'Su total menos 10% + IVA es de: $' + precioDescuento + '\n\n' + 'Lista de precios(unidad): \n'+ lista + '\n' + lista2)
         salida.push ({PrecioFinal: precioDescuento})
-        return salida
     }else{
         alert('Su total + IVA es de: $' + precioIVA + '\n\n' + 'Lista de precios(unidad): \n'+ lista + '\n' + lista2)
         salida.push ({PrecioFinal: precioIVA})
         return salida
     }
+    return salida
 }
 let respuestaQuePrecio = QuePrecio(respuestaQueCantidad)
 console.log (respuestaQuePrecio)
